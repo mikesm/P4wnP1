6b783a36d914e19dee68c67c0ac2e16116c2cb04
mikesm <mikesm@gmail.com>
2018-06-10 23:07:14 +0100

    fix autossh

--- boot/init_autossh.sh
+++ boot/init_autossh.sh
@@ -27,6 +27,6 @@ function start_autossh()
 		echo "    P4wnP1 SSH will be reachable on localhost:$AUTOSSH_REMOTE_PORT on this server"
 		cp $AUTOSSH_PRIVATE_KEY /tmp/ssh_id
 
-		sudo autossh -M 0 -f -T -N  -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i /tmp/ssh_id -R localhost:$AUTOSSH_REMOTE_PORT:localhost:22 $AUTOSSH_REMOTE_USER@$AUTOSSH_REMOTE_HOST
+		sudo autossh -M 0 -f -T -N  -o "ServerAliveInterval=30" -o "ServerAliveCountMax=3" -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i /tmp/ssh_id -R localhost:$AUTOSSH_REMOTE_PORT:localhost:22 $AUTOSSH_REMOTE_USER@$AUTOSSH_REMOTE_HOST
 	fi
 }
